ea7d840a2eeb3e783596ea7e00c49895f977576c
Steve <topkin.steve@gmail.com>
2021-04-14 15:15:28 +0200

    init

---
+++ database/migrations/2021_04_14_124338_create_purchase_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchaseProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_products', function (Blueprint $table) {
+
+            $table->bigInteger('product_id')->unsigned();
+            $table->bigInteger('purchase_id')->unsigned();
+            $table->timestamps();
+
+            $table->primary(array('product_id', 'purchase_id'));
+
+            $table->foreign('purchase_id')->references('id')->on('purchases')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_products');
+    }
+}
